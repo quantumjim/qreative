c04d89800064a8f1db417545400d48d9cf06a273
jwo <jwo@zurich.ibm.com>
2018-09-19 08:45:38 +0200

    expand setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='qreative',
-    version='0.1',
-    scripts=['qreative']
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qreative",
+    version="0.0.1",
+    description="Creative tools for quantum computers",
+    url="https://github.com/quantumjim/creative",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
 )
