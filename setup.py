bfedc42d05ed526ee1daf517b6a81c2e0af43fbc
jwo <jwo@zurich.ibm.com>
2019-02-13 11:17:35 +0200

    change setup stuff

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ VERSION = None
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'numpy', 'matplotlib', 'networkx','pydub'
+    'qiskit', 'qiskit-Aer', 'numpy', 'matplotlib', 'networkx','pydub'
 ]
 
 # What packages are optional?
